5cb01d961ebec9424282a3caefe36f949fac8dd2
Skalyne <theo.kratz.@gmail.com>
2021-05-16 13:20:35 +0200

    WIP add class based views

--- news_site/pages/urls.py
+++ news_site/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, NewsPageView
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', homePageView, name='home'),
+    path('testing', NewsPageView.as_view(), name='testing'),
 ]
--- news_site/pages/views.py
+++ news_site/pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
 def homePageView(request):
     return HttpResponse("Hello world")
+
+class NewsPageView(TemplateView):
+    template_name="home.html"
